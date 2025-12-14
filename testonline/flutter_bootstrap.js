4181d5552c28a91836c1f525eaef56fbe2b3d19d
Hugo Volny <154070269+Blygf@users.noreply.github.com>
2025-12-14 15:30:54 +0100

    new build?

--- testonline/flutter_bootstrap.js
+++ testonline/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3781138506"
+    serviceWorkerVersion: "3582976907"
   }
 });
